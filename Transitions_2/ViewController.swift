2024f6e06eec7be0305f057ca39d0b1beeba97aa
alex livenson <alexlivenson@alexs-MacBook-Pro.local>
2014-11-03 19:12:31 -0800

    Initial Commit

---
+++ Transitions_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transitions_2
+//
+//  Created by alex livenson on 11/3/14.
+//  Copyright (c) 2014 alex.livenson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
